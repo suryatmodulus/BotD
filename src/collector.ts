d249369729cf07157cbd6d2f6f386f6034dd2ddd
Roman Vishnevskii <roman.vishnevskii@fingerprintjs.com>
2021-06-11 18:51:50 +0700

    Add accelerometerPermission signal

--- src/collector.ts
+++ src/collector.ts
@@ -27,6 +27,7 @@ import getFrequency from './sources/frequency'
 import getWindowProperties from './sources/window'
 import getDocumentProperties from './sources/document'
 import getNavigatorProperties from './sources/navigator'
+import requiredAccelerometerPermission from "./sources/accelerometerPermission";
 
 export const enum SignalName {
   UserAgent = 's1',
@@ -58,6 +59,7 @@ export const enum SignalName {
   DocumentProperties = 's27',
   NavigatorProperties = 's28',
   Tag = 's29',
+  AccelerometerPermission = 's30',
 }
 
 export default async function collect(): Promise<SourceResultDict> {
@@ -90,5 +92,6 @@ export default async function collect(): Promise<SourceResultDict> {
     [SignalName.WindowProperties]: getWindowProperties,
     [SignalName.DocumentProperties]: getDocumentProperties,
     [SignalName.NavigatorProperties]: getNavigatorProperties,
+    [SignalName.AccelerometerPermission]: requiredAccelerometerPermission,
   })
 }
